f92cc9041fc5e991a01ca4f0e2e78e88e9f86825
Ibrahim Hamza <ibrahimhamzah007@gmail.com>
2021-08-23 11:38:37 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+// javascript method
+
+// alert("Hello")
+
+// declering variable in js : var, let const
+
+
+// let a = 6
+
+// a = 7
+
+// console.log(a)
+
+// console.error()
+
+// console.warn()
+
+// let b
+
+// b = 22
+
+// console.log(a)
+// console.log(b)
+
+// data type in javascript
+
+/*
+string, number, boolean,
+ null, undefine object,
+*/
+
+const string1 = "SIWES"
+const number = 33
+const number1 = 34.55
+// const undef= undefine
+const bool = true
+const nulll = null
+
+console.log(typeof number1)
+
+// this is method.()
+//this is property
+
+const Person =  {
+    Name: "Aisha Umar",
+    Class: "Primary 4",
+    School: "Add Primary",
+}
+
+// console.log(Person.name)
+
+const SIWES = ['a', 'b', 2, true]
+
+// console.log(SIWES[3])
+
+const firstname = "Ahmad"
+const lastname = "Jibril"
+
+SIWES.push(90)
+console.log(SIWES)
+
+// console.log("My name is "+firstname+" "+lastname)
+// console.log('My name is ${firstname} ${lastname}')
+
+
+const Student =  {
+    Name: "Aisha Umar",
+    Class: "Primary 4",
+    School: "Add Primary",
+    Phone: {
+        id1: 12345,
+        id2: 6789,
+    },
+    Friends : ['Musa', 'Ahmad', 'Fatima'],
+}
+
+// console.log(Student.Phone.id1)
+console.log(Student.Friends[0])
